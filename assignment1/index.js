599b0777dc46bba63b3a251cfc26b10a1496c3b3
GenadyKogan <rgkogan@gmail.com>
2021-07-28 16:48:05 +0300

    test

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,64 +1,101 @@
 const express = require("express"),
-      puppeteer = require('puppeteer'),      
-      videoShow = require('videoshow'),
-      config = require('./input.json'),
-      fs = require('fs'),
-      path = require('path'),
-      app = express(),
-      port = 3000;
-      
+    fs = require("fs"),
+    puppeteer = require("puppeteer"),
+    videoShow = require("videoshow"),
+    bodyParser=require("body-parser").json(),
+    app = express(),
 
+    //8080 - address of the port on which the host server is listening for requests.
+    nPort = 8080,
+    path = require("path"),
+   
+    // video options
+    videoOptions = {
+        fps: 25,
+        loop: 10,
+        transition: false,
+        videoBitrate: 1024,
+        videoCodec: "libx264",
+        size: "640x?",
+        format: "mp4",
+        pixelFormat: "yuv420p",
+        name: "video.mp4"
+    },
 
-      
+    // screenshot the website
+    option={
+      screenshotName: "website.png",
+      path: "website.png", 
+      fullPage: true, 
+      omitBackground: true
+    };
 
-app.get("/", (requset, response) => {
-  response.send("In process...");
+// make screenshot and convert to MP4
+const converting = async(req,res)=>{
+  console.log("Process started");
 
-const takeScreenshot = async()=>{
-  const browser = await puppeteer.launch();
-  const page =await browser
-  .newPage();
-  const option={
-    path: 'images/website.png',
-    fullpage:true,
-    omitBackground: true
-  }
-  await page.goto(config.url)
-  await page.screenshot(option)
-  await page.close()
-  await browser.close()
+  // get url from post request
+  const url = req.body.url; 
 
+  // used the launch() method to create a browser instance
+  const browser = await puppeteer.launch(); 
 
-var images=["images/website.png"]
-let videoOptions={
-    loop:10,
-    fps: 25,
-    transition: true,
-    transitionDuration: 1,
-    videoBitrate: 1024,
-    videoCodec:"libx264",
-    size: "640x?",
-    audioBitrate: "128k",
-    audioChannels: 2,
-    format: "mp4",
-    pixelFormat: "yuv420p",
-    path: "video/video.mp4"
-};
-videoShow(images,videoOptions)
-.save(videoOptions.path)
-.on('start', function(command){
-    console.log("conversion started\t"+command)
-})
-.on('error',function(err,stdout,stderr){
-    console.log("some error occures\t"+err)
-})
-.on('end',function(output){
-    console.log("Conversion complited\t"+ output)
-})
+  // used the newPage() method on the browser object to get the page object
+  const page = await browser.newPage(); 
+
+  // call the goto() method on the page object to load that page
+  await page.goto(url); 
+
+  // meke screenshot
+  await page.screenshot(option);
+
+  // call the close() method on page to close the website
+  await page.close();
+
+  // call the close() method to close the browser
+  await browser.close(); 
+
+  // convert screenshot to MP4
+  videoShow([option.screenshotName], videoOptions) 
+    
+  // save the output as "video.mp4"
+  .save(videoOptions.name) 
+    .on('start', function(command){
+      console.log("Conversion started\t"+command)
+    })
+    .on("error", function (err) {
+      console.error("Some error occures:\t ", err);
+    })
+    .on('end',function(output){
+      console.log("Conversion complited\t"+ output)
+    });
+
+  //  path.join([...paths]) -> Returns: <string> use for saving the path
+  const outputFilePath = path.join(__dirname, videoOptions.name); 
+  
+  // send the result
+  res.send({ file: outputFilePath }); 
 }
 
-var filePath = path.join(__dirname, 'video.mp4');
-fs.writeFileSync(path.resolve(__dirname, 'output.json'), JSON.stringify({file: filePath}));
-//console.log("File Path\t"+filePath);
-takeScreenshot();
+// representation of the specified resource
+// Response: From Server to Client
+// Request : From Client to Server
+app.get("/", (req, res) => {
+  res.send("Get Request Called");
+});
+
+//  submits data to be processed to the identified resource.
+app.post("/",bodyParser,async(req, res) => {
+  try {
+    converting(req,res);
+  } catch (error) {
+    console.log("Some error occures: " + error.message);
+  };
+});
+
+//  bind and listen the connections on the specified host and port
+app.listen(nPort, () => {
+  console.log(
+    `http://localhost:${nPort}`
+  );
 });
---
+++ assignment1/whithoutPostman/index.js
@@ -0,0 +1,62 @@
+const express = require("express"),
+      puppeteer = require('puppeteer'),      
+      videoShow = require('videoshow'),
+      config = require('./input.json'),
+      fs = require('fs'),
+      path = require('path'),
+      app = express(),
+      port = 8080,
+      option={
+        path: 'website.png',
+        fullpage:true,
+        omitBackground: true
+      },
+      videoOptions={
+        loop:10,
+        fps: 25,
+        transition: true,
+        transitionDuration: 1,
+        videoBitrate: 1024,
+        videoCodec:"libx264",
+        size: "640x?",
+        audioBitrate: "128k",
+        audioChannels: 2,
+        format: "mp4",
+        pixelFormat: "yuv420p",
+        path: "video.mp4"
+    },
+    images=["website.png"];
+
+app.get("/", (requset, response) => {
+  response.send("In process...");
+
+const takeScreenshot = async()=>{
+  const browser = await puppeteer.launch();
+  const page =await browser.newPage();
+  await page.goto(config.url)
+  await page.screenshot(option)
+  await page.close()
+  await browser.close()
+  
+videoShow(images,videoOptions)
+.save(videoOptions.path)
+.on('start', function(command){
+    console.log("Conversion started\t"+command)
+})
+.on('error',function(err,stdout,stderr){
+    console.log("some error occures\t"+err)
+})
+.on('end',function(output){
+    console.log("Conversion complited\t"+ output)
+})
+}
+
+const filePath = path.join(__dirname, 'video.mp4');
+fs.writeFileSync(path.resolve(__dirname, 'output.json'), JSON.stringify({file: filePath}));
+//console.log("File Path\t"+filePath);
+takeScreenshot();
+});
+// start the server
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
